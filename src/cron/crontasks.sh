6e6dcf7c4b920b3a609db5bbb40c0c60179691f4
rxxed <raaed@tuta.io>
2021-08-17 19:55:12 +0530

    Relative path for crontasks

--- src/cron/crontasks.sh
+++ src/cron/crontasks.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Change the path if needed
 
-node ~/pccp/src/node/utils/preScraping.js >/dev/null 2>&1
-FILES=$(find ~/pccp/src/node -maxdepth 1 -type f -name "*.js")
+node ../node/utils/preScraping.js >/dev/null 2>&1
+FILES=$(find ../node -maxdepth 1 -type f -name "*.js")
 
 for SCRAPE in $FILES
 do
   node $SCRAPE >/dev/null 2>&1
 done
 
-node ~/pccp/src/node/utils/deleteItems.js >/dev/null 2>&1
-node ~/pccp/src/node/utils/xUpdate.js >/dev/null 2>&1
+node ../node/utils/deleteItems.js >/dev/null 2>&1
+node ../node/utils/xUpdate.js >/dev/null 2>&1
